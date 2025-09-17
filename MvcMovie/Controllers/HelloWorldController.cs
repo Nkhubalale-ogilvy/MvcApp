e01395dccbe0eeafc3798010da34603429dcbfec
Nkhubalale <physimanuel@gmail.com>
2025-09-17 15:28:56 +0200

    assing view only the data to work with

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -5,16 +5,14 @@ namespace MvcMovie.Controllers;
 
 public class HelloWorldController : Controller
 {
-    // 
-    // GET: /HelloWorld/
     public IActionResult Index()
-{
-    return View();
-}
-    // 
-    // GET: /HelloWorld/Welcome/ 
-    public string Welcome(string name, int ID = 1)
     {
-        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        return View();
+    }
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
     }
 }
