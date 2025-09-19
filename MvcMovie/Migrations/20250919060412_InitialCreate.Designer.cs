0cf6028b22372e88434aa32c422fcef0063eae5d
Nkhubalale <physimanuel@gmail.com>
2025-09-19 08:06:53 +0200

    migration update

--- MvcMovie/Migrations/20250919060412_InitialCreate.Designer.cs
+++ MvcMovie/Migrations/20250919060412_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using MvcMovie.Data;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20250917135559_InitialCreate")]
+    [Migration("20250919060412_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -30,6 +30,9 @@ namespace MvcMovie.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("ReleaseDate")
