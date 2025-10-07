7a4d0b6661a693542bb4f79acb8d020f1a2f403f
Nkhubalale <physimanuel@gmail.com>
2025-10-07 08:42:04 +0200

    Update MvcMovieContext to use ApplicationUser instead of IdentityUser

--- MvcMovie/Data/MvcMovieContext.cs
+++ MvcMovie/Data/MvcMovieContext.cs
@@ -5,13 +5,13 @@ using MvcMovie.Models;
 
 namespace MvcMovie.Data
 {
-    public class MvcMovieContext : IdentityDbContext<IdentityUser>
+public class MvcMovieContext : IdentityDbContext<ApplicationUser>
+{
+    public MvcMovieContext(DbContextOptions<MvcMovieContext> options) 
+        : base(options)
     {
-        public MvcMovieContext(DbContextOptions<MvcMovieContext> options) 
-            : base(options)
-        {
-        }
-
-        public DbSet<Movie> Movie { get; set; } = default!;
     }
+
+    public DbSet<Movie> Movie { get; set; } = default!;
+}
 }
